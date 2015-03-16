3183a08c00b86b3b3f4dd8f2a62ad4efe385582b
wski <contact.mattdylan@gmail.com>
2015-03-16 17:00:12 -0400

    Update 2015-03-16T21:00:12.637Z

--- OzoneConfig.js
+++ OzoneConfig.js
@@ -1,7 +1,7 @@
 window.OzoneConfig = {
     "API_URL": 'https://www.owfgoss.org/ng/dev/mp',
     "HELP_URL": 'http://ozone-development.github.io/hud-ui/assets/PlaceholderUserGuide.pdf',
-    "METRICS_URL": "https://www.owfgoss.org:10443/dev/metrics/",
+    "METRICS_URL": "https://www.owfgoss.org:10443/dev/metrics",
     "CENTER_URL": 'http://ozone-development.github.io/center-ui',
     "HUD_URL": "http://localhost:8088/dist",
     "WEBTOP_URL": 'http://ozone-development.github.io/ozp-webtop/',
