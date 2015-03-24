d13e13d24ce1bbc1038d92d180753aebfd253b37
Stephanie Schneider <sschneider713@gmail.com>
2015-03-24 12:48:33 -0400

    Update 2015-03-24T16:48:33.271Z

--- OzoneConfig.js
+++ OzoneConfig.js
@@ -1,8 +1,8 @@
 window.OzoneConfig = {
     "API_URL": 'https://www.owfgoss.org/ng/dev/mp',
-    "HELP_URL": 'http://ozone-development.github.io/hud-ui/assets/PlaceholderUserGuide.pdf',
+    "HELP_URL": 'http://ozone-development.github.io/ozp-hud/assets/PlaceholderUserGuide.pdf',
     "METRICS_URL": "https://www.owfgoss.org:10443/dev/metrics/",
-    "CENTER_URL": 'http://ozone-development.github.io/center-ui',
+    "CENTER_URL": 'http://ozone-development.github.io/ozp-center',
     "HUD_URL": "http://localhost:8088/dist",
     "WEBTOP_URL": 'http://ozone-development.github.io/ozp-webtop/',
     "DEVELOPER_RESOURCES_URL": "#",
