5c581ebe5e45fe319c9880cb1d73234fa3e50dc2
Alan Ward <arw180@gmail.com>
2015-03-31 14:29:45 -0400

    fix OzoneConfig

--- OzoneConfig.js
+++ OzoneConfig.js
@@ -1,11 +1,10 @@
 window.OzoneConfig = {
-    "API_URL": 'https://localhost:8443/marketplace',
-    "HELP_URL": "https://localhost:8088/dist/assets/PlaceholderUserGuide.pdf",
+    "API_URL": 'https://www.owfgoss.org/ng/dev/mp',
+    "HELP_URL": 'http://ozone-development.github.io/ozp-hud/assets/PlaceholderUserGuide.pdf',
     "METRICS_URL": "https://www.owfgoss.org:10443/dev/metrics/",
-    "CENTER_URL": "http://localhost:8000/dist",
+    "CENTER_URL": 'http://ozone-development.github.io/ozp-center',
     "HUD_URL": "http://localhost:8088/dist",
-    "WEBTOP_URL": "http://localhost:9000/",
+    "WEBTOP_URL": 'http://ozone-development.github.io/ozp-webtop/',
     "DEVELOPER_RESOURCES_URL": "#",
     "APP_TITLE": 'OZONE HUD',
-    "FEEDBACK_ADDRESS": "mailto:person@address.com",
 };
